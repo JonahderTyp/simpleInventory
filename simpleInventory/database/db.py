ee7c39413fc782778f2a4dc3d76bd55e134a3f89
Jonah-pInventory <jonah.wille10@gmail.com>
2024-01-15 22:57:30 +0000

    added Deletion rules

--- simpleInventory/database/db.py
+++ simpleInventory/database/db.py
@@ -1,6 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import relationship
-from sqlalchemy import Column, Integer, String, DateTime, Text, ForeignKey
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy import Column, Integer, String, DateTime, Text, ForeignKey, event
 from datetime import datetime
 
 db = SQLAlchemy()
@@ -18,5 +18,20 @@ class Storage(db.Model):
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String)
     parent_id = Column(Integer, ForeignKey('Storage.id'))
-    parent = relationship('Storage', remote_side=[id], backref='children')
+    parent = relationship('Storage', remote_side=[id], backref=backref('children', cascade='all, delete-orphan'))
     items = relationship('Item', back_populates='storage')  # Relationship to Item
+
+def before_delete(mapper, connection, target):
+    # Set parent_id to None for all children of the storage being deleted
+    for child in target.children:
+        child.parent_id = None
+        
+@event.listens_for(Storage, 'before_insert')
+def validate_parent_id(mapper, connect, target):
+    # If parent_id is not None, check if it exists in the database
+    if target.parent_id is not None:
+        exists = db.session.query(db.exists().where(Storage.id == target.parent_id)).scalar()
+        if not exists:
+            raise ValueError(f"Parent ID {target.parent_id} does not exist")
+        
+event.listen(Storage, 'before_delete', before_delete)
